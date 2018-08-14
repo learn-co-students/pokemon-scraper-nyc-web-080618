4663abffd4bbd7d1290600d914b518d328b9ebab
ParkMD7 <parkermdinsmore@gmail.com>
2018-08-14 15:16:46 -0400

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -5,3 +5,8 @@ Scraper.new(@db).scrape
 all_pokemon = @db.execute("SELECT * FROM pokemon;")
 
 # test out your code here!
+
+
+# charmander = Pokemon.new
+# charmeleon = Pokemon.new
+# charzard = Pokemon.new
