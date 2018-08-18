caf0759e1f2a4239bfbe79aaed921087a8bec337
Yan Wang <Yan@Flatirons-Air-5.fios-router.home>
2018-08-17 23:29:08 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,32 @@
+require 'pry'
+
 class Pokemon
+  attr_accessor :hp, :id, :name, :type, :db
+
+  @@all = []
+
+  def initialize(id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+    @@all << self
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
+
+  def self.find(id, db)
+    poke = db.execute("SELECT * FROM pokemon WHERE id = ?",id)[0]
+    pokemon = Pokemon.new(id: id, name: poke[1], type: poke[2], db: db)
+    pokemon.hp = poke[3]
+    pokemon
+  end
+
+  def alter_hp(hp, db)
+    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", hp, self.id)
+  end
 end
+
+# binding.pry
