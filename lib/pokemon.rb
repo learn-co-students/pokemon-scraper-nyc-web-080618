d6dc4133c514fe57ce6696b28fe1315116dc4283
Yan Wang <Yan@Flatirons-Air-5.fios-router.home>
2018-08-17 23:30:03 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,14 +3,11 @@ require 'pry'
 class Pokemon
   attr_accessor :hp, :id, :name, :type, :db
 
-  @@all = []
-
   def initialize(id:, name:, type:, db:)
     @id = id
     @name = name
     @type = type
     @db = db
-    @@all << self
   end
 
   def self.save(name, type, db)
